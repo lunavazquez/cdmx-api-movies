54cd179ea091a0bfb4278540c138bdb050b22745
Mextli <luna.vazquezv@gmail.com>
2019-03-09 20:53:32 -0600

    se agrega funcionalidad para cear la cuenta en firebase

--- src/js/login.js
+++ src/js/login.js
@@ -1,32 +1,55 @@
+const createAccount = (email, password) => {
+  return firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(response => {
+      localStorage.setItem("session", JSON.stringify(response));
+      return (location.href = "index.html");
+    })
+    .catch(error => {
+      alert("Ocurrió un error, intenta más tarde.");
+    });
+};
+
 const login = (email, password) => {
-  return firebase.auth().signInWithEmailAndPassword(email, password).then((response) => {
-    localStorage.setItem('session', JSON.stringify(response));
-    return location.href = 'index.html';
-  }).catch((error) => {
-    alert('Email o contraseña inváldos.');
-    return console.error(error);
-  });
+  return firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(response => {
+      localStorage.setItem("session", JSON.stringify(response));
+      return (location.href = "index.html");
+    })
+    .catch(error => {
+      createAccount(email, password).then(response => {
+        localStorage.setItem("session", JSON.stringify(response));
+        return (location.href = "index.html");
+      });
+    });
 };
 
 const loginGoogle = () => {
   let provider = new firebase.auth.GoogleAuthProvider();
-  return firebase.auth().signInWithPopup(provider).then((response) => {
-    localStorage.setItem('session', JSON.stringify(response));
-    return location.href = 'index.html';
-  }).catch((error) => {
-    alert('Email o contraseña inváldos.');
-    return console.error(error);
-  });
+  return firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(response => {
+      localStorage.setItem("session", JSON.stringify(response));
+      return (location.href = "index.html");
+    })
+    .catch(error => {
+      alert("Email o contraseña inváldos.");
+      return console.error(error);
+    });
 };
 
-document.getElementById('loginForm').addEventListener('submit', (event) => {
+document.getElementById("loginForm").addEventListener("submit", event => {
   event.preventDefault();
   event.stopPropagation();
 
-  const email = document.getElementById('emailInput').value;
-  const password = document.getElementById('passwordInput').value;
+  const email = document.getElementById("emailInput").value;
+  const password = document.getElementById("passwordInput").value;
 
   return login(email, password);
 });
 
-document.getElementById('loginGoogle').addEventListener('click', loginGoogle);
+document.getElementById("loginGoogle").addEventListener("click", loginGoogle);
